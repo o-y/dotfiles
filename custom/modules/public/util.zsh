39cc8299d63fcc0a350d47d27cb0c98351782d12
Suraj Lyons <slyo@google.com>
2022-03-26 01:52:51 +0000

    Fix 't' cmd

--- custom/modules/public/util.zsh
+++ custom/modules/public/util.zsh
@@ -8,9 +8,13 @@ function sa {
 }
 
 function t {
-    if [[ `uname` == 'Darwin' ]]
-    then
-        read -r -p "FYI: You're running this command from your Macbook, continue? [enter] to continue" response
+    # make an assumption. Could check $hostname, but we can assume os x isn't
+    # used as a remote-server over ssh env.
+    if [[ `uname` == 'Darwin' ]] then
+        if ! read -q "choice?FYI: You're running this command on localhost, continue? [y/N] "; then
+            echo -e '\r\033[0;32mOK! Exiting...\033[0m\c'
+            exit 1;
+        fi
     fi
 
     tmux -CC attach-session -t tmuxssh || tmux -CC new-session -s tmuxssh
