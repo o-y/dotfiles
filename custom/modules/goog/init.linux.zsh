d4ecb9b36edf0864e9528e540a31e8a3890c773a
zv <zv@zv.wtf>
2023-05-13 18:43:49 +0100

    add some basic handling to generify these files for other linux hosts

--- custom/modules/goog/init.linux.zsh
+++ custom/modules/goog/init.linux.zsh
@@ -1,2 +1,5 @@
-source /etc/bash_completion.d/hgd
-source /etc/bash_completion.d/g4d
+# it would be worth considering a more maintable way of handling files scoped to specific hostnames
+if [[ $(hostname) == "slyo.lon.corp.google.com" ]]; then
+    source /etc/bash_completion.d/hgd 2> /dev/null
+    source /etc/bash_completion.d/g4d 2> /dev/null
+fi
