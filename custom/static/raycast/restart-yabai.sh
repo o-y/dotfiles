5bde4a588a51ee021d9221c76c2dc3dd7c808588
zv <zv@zv.wtf>
2023-06-12 00:10:14 +0100

    add yabai raycast plugin

---
+++ custom/static/raycast/restart-yabai.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Yabai
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ⌨️
+# @raycast.packageName Restart Yabai
+
+# Documentation:
+# @raycast.description Restart Yabai
+# @raycast.author zv
+# @raycast.authorURL zv.wtf
+
+launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai"
