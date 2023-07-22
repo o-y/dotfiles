79a8ae31890f9813a6481b064f1cd8300c650001
o-y <zv@zv.wtf>
2023-07-22 14:04:08 +0100

    update env

--- custom/modules/public/prompt.zsh
+++ custom/modules/public/prompt.zsh
@@ -9,6 +9,16 @@ COLOR_GIT=$'\e[38;5;219m'
 
 NEWLINE=$'\n'
 COLOR_DEF=$'\e[38;5;250m'
+
 setopt PROMPT_SUBST
 
-export PROMPT='%B${COgLOR_USR}%n${COLOR_AT}@${COLOR_USR}%m ${COLOR_DIR}%~ ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}~%b '
+PROMPT='%B'                                # start of bold sequence
+PROMPT+="${COLOR_USR}%n"                   # display username
+PROMPT+="${COLOR_AT}@"                     # display @ symbol
+PROMPT+="${COLOR_USR}%m"                   # display hostname
+PROMPT+=" ${COLOR_DIR}%~"                  # display current directory
+PROMPT+=" ${COLOR_GIT}$(parse_git_branch)" # display git branch
+PROMPT+="${COLOR_DEF}${NEWLINE}"           # reset colour and add newline
+PROMPT+='~%b '                             # display tilde
+
+export PROMPT
