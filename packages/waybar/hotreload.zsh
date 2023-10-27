74a5d4bee4899a22cf46b053453d7f9728ddf68a
zv <zv@zv.wtf>
2023-10-27 18:13:41 +0100

    add actual waybar styling and playerctl infra

--- packages/waybar/hotreload.zsh
+++ packages/waybar/hotreload.zsh
@@ -1,10 +1,14 @@
 DIRECTORY_TO_WATCH="$HOME/dotfiles/packages/waybar/"
 
+stylus ./
+pkill waybar
 waybar &
 
 while true; do
   inotifywait -e modify -r $DIRECTORY_TO_WATCH
   
+  stylus ./
   pkill waybar
-  waybar &
+  waybar  &
 done
+
