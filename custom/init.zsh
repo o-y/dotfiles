86a0fd21c1e24b0edfb0eff4e35d8801f42ba0dd
o-y <zv@zv.wtf>
2023-11-12 14:55:33 +0000

    add logic to check if files are encrypted

--- custom/init.zsh
+++ custom/init.zsh
@@ -1,14 +1,28 @@
 PATH_TO_SCRIPT=$(realpath -s "$0")
 MODULES_DIR=$(dirname "$PATH_TO_SCRIPT")/modules
 
-# sources the specified file if it matches the hostname
+# sources the specified file if it matches the hostname and isn't encrypted
+is_encrypted() {
+  file="$1"
+  if command -v git-crypt &> /dev/null; then
+    git-crypt status --encrypted "$file" &> /dev/null
+  else
+    echo "[init] WARNING - git-crypt not installed on system, delegating to naive check"
+    head -n 1 "$file" | grep -q '^GITCRYPT'
+  fi
+}
+
 source_helper() {
   file="$1"
   file_uname="$2"
 
   if [[ $(uname) == $file_uname ]]; then
-    source "$file"
-  fi;
+    if ! is_encrypted "$file"; then
+      source "$file"
+    else
+      echo "[init] WARNING - skipping encrypted file: $file"
+    fi
+  fi
 }
 
 for dir in public private goog; do
