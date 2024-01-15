e28ddeeb6c6a0bfe142739d8f303a29873a1ed18
zv <zv@zv.wtf>
2024-01-15 20:29:02 +0000

    fix prompt git branch not updating

--- custom/modules/public/prompt.zsh
+++ custom/modules/public/prompt.zsh
@@ -10,15 +10,15 @@ COLOR_GIT=$'\e[38;5;219m'
 NEWLINE=$'\n'
 COLOR_DEF=$'\e[38;5;250m'
 
-setopt PROMPT_SUBST
+precmd() {
+    PROMPT='%B'                                # start of bold sequence
+    PROMPT+="${COLOR_USR}%n"                   # display username
+    PROMPT+="${COLOR_AT}@"                     # display @ symbol
+    PROMPT+="${COLOR_USR}%m"                   # display hostname
+    PROMPT+=" ${COLOR_DIR}%~"                  # display current directory
+    PROMPT+=" ${COLOR_GIT}$(parse_git_branch)" # display git branch
+    PROMPT+="${COLOR_DEF}${NEWLINE}"           # reset color and add newline
+    PROMPT+='~%b '                             # display tilde
 
-PROMPT='%B'                                # start of bold sequence
-PROMPT+="${COLOR_USR}%n"                   # display username
-PROMPT+="${COLOR_AT}@"                     # display @ symbol
-PROMPT+="${COLOR_USR}%m"                   # display hostname
-PROMPT+=" ${COLOR_DIR}%~"                  # display current directory
-PROMPT+=" ${COLOR_GIT}$(parse_git_branch)" # display git branch
-PROMPT+="${COLOR_DEF}${NEWLINE}"           # reset colour and add newline
-PROMPT+='~%b '                             # display tilde
-
-export PROMPT
+    export PROMPT
+}
