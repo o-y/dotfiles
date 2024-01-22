883dba1bb42c141f7d6416bb0b6421633b64f231
zv <zv@zv.wtf>
2024-01-22 16:33:26 +0000

    remove shaders, update to hyprland 0.32.0 from 0.23

--- packages/hypr/shaders/blue-light.glsl
+++ packages/hypr/shaders/blue-light.glsl
@@ -2,11 +2,11 @@ precision mediump float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
 
-const float temperature = 3600.0;
+const float temperature = 4000.0;
 const float temperatureStrength = 1.0;
 
 #define WithQuickAndDirtyLuminancePreservation
-const float LuminancePreservationFactor = 1.0;
+const float LuminancePreservationFactor = 3.0;
 
 // function from https://www.shadertoy.com/view/4sc3D7
 // valid from 1000 to 40000 K (and additionally 0 for pure full white)
