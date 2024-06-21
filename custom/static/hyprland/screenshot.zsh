c0d73fff4211c714e231329215e034a1978beb75
zv <zv@zv.wtf>
2024-06-21 14:49:26 +0100

    refactor static/images, add kando config

--- custom/static/hyprland/screenshot.zsh
+++ custom/static/hyprland/screenshot.zsh
@@ -1,3 +1,13 @@
 # Capture a screenshot and saved it to the clipboard and the filesystem
 
-grimblast copysave area ~/Pictures/Screenshots/$(date +"%d-%m-%y_%T").png
+image_specifier=$(date +"%d-%m-%y_%T")
+screenshot_path="$HOME/Pictures/Screenshots/$image_specifier.png"
+
+grimblast copysave area $screenshot_path
+
+# Notification
+dunstify \
+  -a "Open Screenshot" \
+  -i "$screenshot_path" \
+  "$image_specifier.png" \
+  "Click to open in Chrome"
