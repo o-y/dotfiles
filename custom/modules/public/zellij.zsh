2b7a3bd98bb16ab09080df20f5aba5f573678f67
o-y <zv@zv.wtf>
2025-01-17 04:04:05 +0000

    update zellij status

--- custom/modules/public/zellij.zsh
+++ custom/modules/public/zellij.zsh
@@ -10,3 +10,19 @@ function zr() {
 function hxz() {
     zellij edit "$@"
 }
+
+# updating tab
+zellij_tab_name_update() {
+    if [[ -n $ZELLIJ ]]; then
+        local current_dir=$PWD
+        if [[ $current_dir == $HOME ]]; then
+            current_dir="~"
+        else
+            current_dir=${current_dir##*/}
+        fi
+        command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+    fi
+}
+
+zellij_tab_name_update
+chpwd_functions+=(zellij_tab_name_update)
