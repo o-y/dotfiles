9cb573309cdb5c197936cfcf75169ef54dc70b34
zv <zv@zv.wtf>
2025-05-08 18:42:00 +0100

    add jj config, remove hypr-empty

--- custom/modules/goog/init.linux.zsh
+++ custom/modules/goog/init.linux.zsh
@@ -1,10 +1,9 @@
 if [[ $(hostname) == *corp.google.com || $(hostname) == *c.googlers.com ]]; then
     source /etc/bash_completion.d/hgd 2> /dev/null
     source /etc/bash_completion.d/g4d 2> /dev/null
+    source /etc/bash_completion.d/jjd 2> /dev/null
 
     if [ -d /google/data ]; then
         fpath=(/google/src/files/head/depot/google3/devtools/blaze/scripts/zsh_completion $fpath)
-        zstyle ':completion:*' use-cache on
-        zstyle ':completion:*' cache-path ~/.zsh/cache
     fi
 fi
