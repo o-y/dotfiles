e396c3d6b7b7debd5daa274b9f8ee48dc6990557
zv <zv@zv.wtf>
2025-05-08 21:21:27 +0100

    add nvm (unfortunately) and hypridle config

--- custom/modules/public/setup.zsh
+++ custom/modules/public/setup.zsh
@@ -77,3 +77,10 @@ if ! type pyenv &> /dev/null; then
         eval "$(pyenv init - zsh)"
     fi
 fi
+
+# nvm
+if [ -e "$HOME/.nvm" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+fi
