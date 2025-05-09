ef702a04a5674ab03a551fe6ca554456359ec262
o-y <zv@zv.wtf>
2025-05-09 01:57:03 +0100

    added some nvim config, updated ghostty theme, added wallpapers

--- packages/nvim/init.lua
+++ packages/nvim/init.lua
@@ -35,3 +35,18 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { 
+    mouse = true,
+    border = true
+  })
+end, {})
--- packages/nvim/lua/plugins/init.lua
+++ packages/nvim/lua/plugins/init.lua
@@ -13,6 +13,27 @@ return {
     end,
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true
+      },
+    },
+  },
+
+  { "nvzone/volt", lazy = true },
+  { "nvzone/menu" , lazy = true },
+
+  {
+    "nvzone/minty",
+    cmd = { "Shades", "Huefy" },
+  },
+
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
