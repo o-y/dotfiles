940a4d61e5eb8a0cf83936cc9690ca2639a1d94c
o-y <zv@zv.wtf>
2025-06-16 02:58:52 +0100

    add infra to suspend tmux, and other various additions

--- custom/modules/public/exports.zsh
+++ custom/modules/public/exports.zsh
@@ -5,7 +5,7 @@ export TERM=xterm-256color
 export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
 
 #### Editor
-export EDITOR="hx"
+export EDITOR="nvim"
 
 #### Required for Conda which overrides clear - https://askubuntu.com/a/1402408
 export TERMINFO=/usr/share/terminfo
@@ -13,16 +13,15 @@ export TERMINFO=/usr/share/terminfo
 #### Zsh History
 HISTFILE=~/.zsh_history
 
-setopt NO_INC_APPEND_HISTORY_TIME
-setopt NO_INC_APPEND_HISTORY
 
-# In-memory per-session state
-HISTSIZE=1000000
-
-# Number of entries to save in ~/.zsh_history
-SAVEHIST=1000000
-
+# setopt NO_INC_APPEND_HISTORY_TIME
+# setopt NO_INC_APPEND_HISTORY
 setopt EXTENDED_HISTORY
 setopt SHAREHISTORY
 setopt APPEND_HISTORY
 setopt INC_APPEND_HISTORY
+setopt INC_APPEND_HISTORY_TIME
+
+
+HISTSIZE=1000000 # In-memory per-session state
+SAVEHIST=1000000 # Number of entries to save in ~/.zsh_history
