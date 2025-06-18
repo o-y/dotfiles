12a547996456bdeb6568ca9e885e4d2145caf652
o-y <zv@zv.wtf>
2025-06-18 02:19:56 +0100

    modify nvim config

--- packages/nvim/lua/chadrc.lua
+++ packages/nvim/lua/chadrc.lua
@@ -6,30 +6,51 @@ return {
 
 	base46 =  {
 		theme = "bearded-arc",
+		theme_toggle = { 
+			"bearded-arc", -- dark theme
+			"one_light" -- light theme
+		},
 	},
 
 	nvdash = {
 		load_on_startup = true,
 
 		header = {
-			"                            ",
-			"     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
-			"   ▄▀███▄     ▄██ █████▀    ",
-			"   ██▄▀███▄   ███           ",
-			"   ███  ▀███▄ ███           ",
-			"   ███    ▀██ ███           ",
-			"   ███      ▀ ███           ",
-			"   ▀██ █████▄▀█▀▄██████▄    ",
-			"     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
-			"                            ",
-			"     Powered By  eovim    ",
-			"                            ",
+			"                                                                   ",
+			"      ████ ██████           █████      ██                    ",
+			"     ███████████             █████                            ",
+			"     █████████ ███████████████████ ███   ███████████  ",
+			"    █████████  ███    █████████████ █████ ██████████████  ",
+			"   █████████ ██████████ █████████ █████ █████ ████ █████  ",
+			" ███████████ ███    ███ █████████ █████ █████ ████ █████ ",
+			"██████  █████████████████████ ████ █████ █████ ████ ██████",
+			"",
+		},
+
+		buttons = {
+			{ txt = "  Find Files", keys = "ff", cmd = "Telescope find_files" },
+			{ txt = "󰈭  Find Text", keys = "fw", cmd = "Telescope live_grep" },
+			{ txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+			{ txt = "  View Mappings", keys = "ch", cmd = "NvCheatsheet" },
+
+			{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+			{
+				txt = function()
+					local stats = require("lazy").stats()
+					local ms = math.floor(stats.startuptime) .. " ms"
+					return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+				end,
+				hl = "NvDashFooter",
+				no_gap = true,
+			},
+			{ txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
 		},
 	},
 
 	ui = {
 		tabufline = {
-			lazyload = false,
+			enabled = true,
+			lazyload = true,
 		},
 	},
 }
