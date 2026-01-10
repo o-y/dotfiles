a1566c60bfcdf25d4a29d4e4fe03ef14c6be3823
o-y <zv-corp@zv.wtf>
2026-01-10 15:55:16 +0000

    various fixes

--- custom/modules/public/alias.zsh
+++ custom/modules/public/alias.zsh
@@ -3,16 +3,10 @@ alias pip=pip3
 alias f=fuck
 alias c=clear
 
-# This is a workaround for the `clear` command being
-# overridden in some environments (e.g. Pixi, etc.)
-function clear() {
-    if [[ -e /usr/bin/clear ]]; then
-        /usr/bin/clear
-        return
-    fi
-    if [[ -e /bin/clear ]]; then
-        /bin/clear
-        return
-    fi
-    clear
-}
+# suffix alias
+alias -s json="fx"
+alias -s md="cat"
+
+# clean
+alias hoover="ps -u $(whoami) -o pid,comm | grep -E '/Applications/|/Users/' | grep -vE 'Google Chrome|Ghostty|Terminal' | awk '{print \$1}' | xargs kill -9 2>/dev/null"
+alias dry-hoover="ps -u $(whoami) -o pid,comm | grep -E '/Applications/|/Users/' | grep -vE 'Google Chrome|Ghostty|Terminal' | awk '{print \$1}' | xargs ps -p"
